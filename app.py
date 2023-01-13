ed03e3297f2208657b8b02d5f628f6c9a8e43eed
hussein <hzh09@mail.aub.edu>
2023-01-13 23:10:01 +0200

    message

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask 
+from flask import Flask, jsonify,request 
 import os
+from google.cloud import firestore
+db = firestore.Client()
 app = Flask(__name__)
+
 
 
 @app.route('/')
 def home():
     return "hello"
+@app.route('/adduser',methods = ["POST"])
+def adduser():
+    data = request.get_json()
+    doc = db.collection('users').document().set(data)
+    return "done"
+
+@app.route('/users')
+def getusers():
+    items = []
+    docs = db.collection('users').stream()
+    for doc in docs:
+        items.append(doc.to_dict())
+    return jsonify(items)
+
+
 
 if __name__ == '__main__':
     app.run(port=os.environ.get("port",5000),host="0.0.0.0")
